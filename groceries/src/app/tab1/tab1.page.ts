f83dce780811266e9c580ed5e8d6a55ebcab6d1d
DeclannH <69803173+DeclannH@users.noreply.github.com>
2023-11-19 23:02:01 -0500

    Got edit working - final commit with screencasts
    Screen casts and final edits to edit - push to origin and commit to main

--- groceries/src/app/tab1/tab1.page.ts
+++ groceries/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, ToastController } from '@ionic/angular';
+import { NavController, ToastController, AlertController } from '@ionic/angular';
+
 
 
 @Component({
@@ -14,37 +15,60 @@ export class Tab1Page {
 
 
   
-constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
 }
 
+itemName = ""
+
 items = [
-  {Number:1,
-    name: "Milk"},
+ "Milk",
 
-  {Number:2,
-    name: "Bread"},
+  "Bread",
 
-  {Number:3,
-    name: "Eggs"},
+  "Eggs",
 
-  {Number:4,
-    name: "Chickens **NO ROOSTERS**"}
+  "Chickens **NO ROOSTERS**"
 
 ]
 
+itemCount = 4
 
 public removeItem(index:number){
   let id =this.items[index]
   let newArray = this.items.filter((e, i) => i !== index);
   this.items = newArray
+
+  this.itemCount -= 1
+}
+
+addItem() {
+  if (this.itemName.length > 0) {
+    let item = this.itemName;
+    this.items.push(item);
+    this.itemName = "";
+
+  }
+}
+
+async updateItem(index:number) {
+  let alert = await this.alertCtrl.create({
+    header: 'Update Item?',
+    message: 'Type in your new item to update.',
+    inputs: [{ name: 'editItem', placeholder: 'Item' }],
+    buttons: [{ text: 'Cancel', role: 'cancel' },
+              { text: 'Update', handler: data => {  
+                this.items[index] = data.editItem; }}]
+  });
+await alert.present();
 }
 
 
-addItem(): void{
-  console.log("Adding Item");
-
+//addItem(): void{
+  //console.log("Adding Item");
+  //this.itemCount += 1
+  //this.items.push(this.itemName)
   
-}
+//}
 
 public addButtons = ['OK'];
   public addInputs = [
